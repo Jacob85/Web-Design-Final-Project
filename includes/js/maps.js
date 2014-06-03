847f0659298feba46476e41301d0be4b9db68174
Jacob85 <yaki.ams@gmail.com>
2014-06-03 13:37:59 +0300

    Added marker with image

--- includes/js/maps.js
+++ includes/js/maps.js
@@ -1,6 +1,9 @@
+
 /**
  * Created by Jacob on 6/3/14.
  */
+var map ;
+
 function initialize()
 {
     var mapOptions = {
@@ -8,7 +11,19 @@ function initialize()
         zoom: 12
     };
     /*Create new Google Map element*/
-    var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+    map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+    /* Adding a marker to the map */
+    var image = 'includes/images/thumb_Facebook_icon.png';
+    var myLatlng = new google.maps.LatLng(32.06, 34.77);
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        icon: image,
+        animation: google.maps.Animation.DROP, /*Animation drop */
+        title:"Hello World!"   /*tooltip message */
+    });
+
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
