7700c0993b3519ee3cf0817f0399163c7f8ddda6
eyalkd <eyalkd@hotmail.com>
2014-06-04 16:24:08 +0300

    fix map view & delete phpstorm idea files

--- includes/js/maps.js
+++ includes/js/maps.js
@@ -8,7 +8,7 @@ function initialize()
 {
     var mapOptions = {
         center: new google.maps.LatLng(32.06, 34.77),/*Tel Aviv*/
-        zoom: 12
+        zoom: 13
     };
     /*Create new Google Map element*/
     map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
@@ -23,7 +23,45 @@ function initialize()
         title:"Hello World!"   /*tooltip message */
     });
 
+    map.setOptions({styles: styles});
 
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+var styles  = [
+    {
+        "elementType": "geometry.fill",
+        "stylers": [
+            { "saturation": -89 },
+            { "weight": 0.4 },
+            { "visibility": "on" },
+            { "invert_lightness": true },
+            { "hue": "#ff0000" }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            { "saturation": -89 },
+            { "hue": "#ff0000" },
+            { "weight": 0.1 },
+            { "color": "#333333" },
+            { "visibility": "simplified" }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+            { "visibility": "off" }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            { "weight": 0.3 }
+        ]
+    }];
+
+
